662d7a48dc408e7717a52bf0df1b61f3504d1e0c
EV9H <106489113+EV9H@users.noreply.github.com>
2022-07-30 22:58:42 -0400

    historical data obtaining completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import influxdb_client
 import os
 import time
+
+import pandas
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 # pip install yfinance --upgrade --no-cache-dir
@@ -9,7 +11,7 @@ import yfinance as yf
 
 # TEST FUNCTIONS
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Initialize Client
 # token = os.environ.get("INFLUXDB_TOKEN")
@@ -20,33 +22,31 @@ url = "https://us-east-1-1.aws.cloud2.influxdata.com"
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
 # get yahoo finance data
-startDateStr = '2022-07-25'
+startDateStr = '2022-07-01'
 endDateStr = '2022-07-30'
 instrumentIds = ['IBM']
 ds = yf.download(tickers='IBM',
-                 start=startDateStr,  end=endDateStr)
-data = ds['Adj Close']
-
-for index in data.index:
-    print(index)
-    print(data[index])
+                 start=startDateStr, end=endDateStr)
+stockData = ds['Adj Close']
 
 # Write Data
-bucket = "randomWalk"
+bucket = "financeData"
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
+for timestamp in stockData.index:
+    val = stockData[timestamp]
+    timestamp_datetime_UTC = pandas.Timestamp.to_pydatetime(timestamp).isoformat("T") + "Z"
+    print(timestamp_datetime_UTC, val)
+    point = (
+        Point("Stocks")
+            .tag("Ticker", "IBM")
+            .field("AdjClosed", val)
+            .time(timestamp_datetime_UTC)
+    )
+    write_api.write(bucket=bucket, org="evanhuanghz@gmail.com", record=point)
 
-
-# for value in data:
-#     point = (
-#         Point("measurement1")
-#         .tag("Ticker", "IBM")
-#         .field("AdjClosed", value)
-#     )
-#     write_api.write(bucket=bucket, org="evanhuanghz@gmail.com", record=point)
-#     print(value)
-#     time.sleep(0.1)  # separate points by 1 second
+    # time.sleep(1)  # separate points by 1 second
 
 '''
 start = 0
@@ -64,15 +64,18 @@ while True:
     time.sleep(5)  # separate points by 1 second
 '''
 
-val = random.randint(0,100)
-data = (
-    Point("measurement1")
-    .tag("name", "random1")
-    .field("value", val)
-    .time(datetime.datetime.now())
-)
-
-
+""" convert date time to utc """
+# val = 9999
+# datetime = datetime.utcnow() - timedelta(days = 2)
+# time_minus_1d = datetime.isoformat("T") + "Z"
+#
+# point = (
+#     Point("measurement1")
+#     .tag("name", "random1")
+#     .field("value", val)
+#     .time(time_minus_1d)
+# )
+# write_api.write(bucket=bucket, org="evanhuanghz@gmail.com", record=point)
 
 # # Flux Query
 #
